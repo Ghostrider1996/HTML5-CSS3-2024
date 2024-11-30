aa55d6768631c8493619b113e6ea64ea3a5ccb8a
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2024-11-30 18:48:53 +0200

    initial commit

--- js-scripts/var-sub-dropdown-menu.js
+++
@@ -1,5 +0,0 @@
-const subButton = document.querySelector('.dropdown-button'); // Update this selector to match your button
-const subDropDown = document.querySelector('#sub-drop-down'); // Update this selector
-subButton.addEventListener('click', function () { // Change menuStatus to subButton
-    subDropDown.classList.toggle('active-2');
-});
